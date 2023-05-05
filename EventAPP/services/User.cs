a36ac48df16a2a3f355fa217a7f7d12d04dd5fd5
Adnan Macanovic Omerbasic <adna0180@edu.zealand.dk>
2023-05-05 14:57:21 +0200

    Implementering af Log Ind system, Admin, Studerende

---
+++ EventAPP/services/User.cs
@@ -0,0 +1,26 @@
+﻿namespace EventAPP.services
+{
+    public class User : IUser
+    {
+        public bool IsUserAdmin => throw new NotImplementedException();
+
+        public string UserName => throw new NotImplementedException();
+
+        public bool IsLoggedIn => throw new NotImplementedException();
+
+        public bool CheckLogIn(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetUserLoggedIn(string name, bool isAdmin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UserLoggedOut()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
