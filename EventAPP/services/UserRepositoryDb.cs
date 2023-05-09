34935c9aed35e14103e84c0772da35da994a42e7
Adnan Macanovic Omerbasic <adna0180@edu.zealand.dk>
2023-05-09 13:52:44 +0200

    dflflflfl

--- EventAPP/services/UserRepositoryDb.cs
+++ EventAPP/services/UserRepositoryDb.cs
@@ -2,109 +2,109 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Data.SqlClient;
 
-namespace EventAPP.services
-{
-    public class UserRepositoryDb : IUserRepository
-    {
+//namespace EventAPP.services
+//{
+//    public class UserRepositoryDb : IUserRepository
+//    {
 
-        private User ReadUser(SqlDataReader reader)
-        {
-            User _user = new User();
+//        private User ReadUser(SqlDataReader reader)
+//        {
+//            User _user = new User();
 
-            _user.Email = reader.GetString(1);
-            _user.Password = reader.GetString(2);
-            _user.IsAdmin = reader.GetBoolean(3);
-
-
-            return _user;
-        }
-
-        public bool IsUserAdmin
-        {
-            get
-            {
-                SqlConnection conn = new SqlConnection(DbServer.GetConnectionString);
-                conn.Open();
+//            _user.Email = reader.GetString(1);
+//            _user.Password = reader.GetString(2);
+//            _user.IsAdmin = reader.GetBoolean(3);
 
 
-                String sql = "Select IsAdmin From User Where Email = @Email";
+//            return _user;
+//        }
 
-                SqlCommand cmd = new SqlCommand(sql, conn);
+//        public bool IsUserAdmin
+//        {
+//            get
+//            {
+//                SqlConnection conn = new SqlConnection(DbServer.GetConnectionString);
+//                conn.Open();
 
 
-                cmd.Parameters.AddWithValue("@Email", "AdnanTest");
+//                String sql = "Select IsAdmin From User Where Email = @Email";
+
+//                SqlCommand cmd = new SqlCommand(sql, conn);
+
+
+//                cmd.Parameters.AddWithValue("@Email", "AdnanTest");
                 
-                SqlDataReader userReader = cmd.ExecuteReader();
+//                SqlDataReader userReader = cmd.ExecuteReader();
 
-                if (userReader.Read())
-                {
-                    return userReader.GetBoolean(0);
-                }
-                return userReader.GetBoolean(1);
+//                if (userReader.Read())
+//                {
+//                    return userReader.GetBoolean(0);
+//                }
+//                return userReader.GetBoolean(1);
 
-            }
+//            }
 
-        }
-
-
-        public string UserName
-        {
-            get
-            {
-                SqlConnection conn = new SqlConnection(DbServer.GetConnectionString);
-                conn.Open();
-
-                String sql = "Select * From User Where Email = @Email";
+//        }
 
 
-                SqlCommand cmd = new SqlCommand(sql, conn);
+//        public string UserName
+//        {
+//            get
+//            {
+//                SqlConnection conn = new SqlConnection(DbServer.GetConnectionString);
+//                conn.Open();
 
-                cmd.Parameters.AddWithValue("@Email", "Admin");
-
-
-                SqlDataReader userName = cmd.ExecuteReader();
-
-                if (userName.Read())
-                {
-                    return userName.GetString(0);
-                }
-                return userName.GetString(1);
-            }
-        }
-
-        public bool IsLoggedIn
-        {
-            get
-            {
-                SqlConnection conn = new SqlConnection(DbServer.GetConnectionString);
-                conn.Open();
-
-                String sql = "Select * From User";
+//                String sql = "Select * From User Where Email = @Email";
 
 
-                SqlCommand cmd = new SqlCommand(sql, conn);
+//                SqlCommand cmd = new SqlCommand(sql, conn);
 
-                SqlDataReader loggedin = cmd.ExecuteReader();
+//                cmd.Parameters.AddWithValue("@Email", "Admin");
 
-                if (loggedin.Read())
-                {
-                    return loggedin.GetBoolean(0);
-                }
-                return loggedin.GetBoolean(1);
-            }
-        }
 
-        public void SetUserLoggedIn(string email, bool isAdmin)
-        {
-            SqlConnection conn = new SqlConnection(DbServer.GetConnectionString);
-            conn.Open();
+//                SqlDataReader userName = cmd.ExecuteReader();
 
-            String sql = "Select "
-        }
+//                if (userName.Read())
+//                {
+//                    return userName.GetString(0);
+//                }
+//                return userName.GetString(1);
+//            }
+//        }
 
-        public void UserLoggedOut()
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
+//        public bool IsLoggedIn
+//        {
+//            get
+//            {
+//                SqlConnection conn = new SqlConnection(DbServer.GetConnectionString);
+//                conn.Open();
+
+//                String sql = "Select * From User";
+
+
+//                SqlCommand cmd = new SqlCommand(sql, conn);
+
+//                SqlDataReader loggedin = cmd.ExecuteReader();
+
+//                if (loggedin.Read())
+//                {
+//                    return loggedin.GetBoolean(0);
+//                }
+//                return loggedin.GetBoolean(1);
+//            }
+//        }
+
+//        public void SetUserLoggedIn(string email, bool isAdmin)
+//        {
+//            SqlConnection conn = new SqlConnection(DbServer.GetConnectionString);
+//            conn.Open();
+
+//            String sql = "Select "
+//        }
+
+//        public void UserLoggedOut()
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
